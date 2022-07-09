6d4e4d5df9ee6ce12e7bd9ccc1c6a0fcd64f4fd8
RockyChung <mrrockychung@gmail.com>
2022-07-09 23:47:05 +0800

    Spring初始設定

---
+++ spring-spring-20220709/src/main/java/com/example/demo/SpringSpring20220709Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpring20220709Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpring20220709Application.class, args);
+	}
+
+}
---
+++ spring-spring-20220709/src/test/java/com/example/demo/SpringSpring20220709ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpring20220709ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
